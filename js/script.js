05598fca162518dcd8dbc2ad03bb0db00f0f4953
sj970329 <xox_s2@naver.com>
2021-11-16 16:19:36 +0900

    .
    edit

--- js/script.js
+++ js/script.js
@@ -4,6 +4,24 @@ $('.select').click(
     }
 );
 
+
+$(".menu__group li a").click(
+  function(){
+      var $sub = $(this).children(".sub").hasClass("on");
+  
+      if($sub){
+          $(this).children(".sub").stop().slideUp();
+          $(this).children(".sub").removeClass("on");
+      }else{
+          $(this).children(".sub").stop().slideDown();
+          $(this).children(".sub").addClass("on");
+          $(this).siblings().children(".sub").stop().slideUp();
+          $(this).siblings().children(".sub").removeClass("on");
+      }
+  }
+);
+
+
 (function(){
     'use strict';
   
